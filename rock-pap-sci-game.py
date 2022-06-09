3f8ccc53bb59852af95db2638a0807e6d35816cb
mcgue <mcgu@protonmail.com>
2022-06-09 13:16:47 -0700

    add while loops to redo if a tie, add sign-off

--- rock-pap-sci-game.py
+++ rock-pap-sci-game.py
@@ -5,34 +5,49 @@ choices = ['ROCK', 'PAPER', 'SCISSORS']
 
 # Introduce game and get input
 print('Let\'s play Rock, Paper, Scissors :)')
-player_choice = input('Please enter your choice:')
 
-# Move to all uppercase
-player_choice = player_choice.upper()
+# While loop to restart when get tie
+while True:
+    player_choice = input('Please enter your choice: ')
 
-# Check guess for validity
-while player_choice not in choices:
-    player_choice = input('Please enter ROCK, PAPER, or SCISSORS:')
+    # Move to all uppercase
     player_choice = player_choice.upper()
+
+    # Check guess for validity
+    while player_choice not in choices:
+        player_choice = input('Please enter ROCK, PAPER, or SCISSORS:')
+        player_choice = player_choice.upper()
     
-computer_choice = choices[random.randint(0, 2)]
+    computer_choice = choices[random.randint(0, 2)]
 
-if player_choice == computer_choice:
-    print('It is a tie!')
+    if player_choice != computer_choice:
+        break
 
-elif player_choice == 'ROCK':
-    if computer_choice.upper() == 'SCISSORS':
-        print('You win!')
+    if player_choice == computer_choice:
+        print('It is a tie!')
+    player_answer = input('Try again? Enter Y or N ')
+    if player_answer.upper() == 'N' or player_choice != computer_choice:
+        break
+
+if player_choice != computer_choice:
+    if player_choice == 'ROCK':
+        if computer_choice.upper() == 'SCISSORS':
+            print('You win!')
+        else:
+             print('You lose')
+    elif player_choice == 'PAPER':
+        if computer_choice.upper() == 'ROCK':
+            print('You win!')
+        else:
+            print('You lose')
+    elif player_choice == 'SCISSORS':
+        if computer_choice.upper() == 'PAPER':
+            print('You win!')
+        else:
+            print('You lose')
     else:
-        print('You lose')
-elif player_choice == 'PAPER':
-    if computer_choice.upper() == 'ROCK':
-        print('You win!')
-    else:
-        print('You lose')
-else:
-    print('still calculating')
+        print('still calculating')
 
-# Print result
-print('I picked ' + computer_choice + '!')
-
+    # Print result
+    print('I picked ' + computer_choice + '!')
+print('Thanks for playing!')
