1b39d29205346ba17ca919b901c3187dfb0298bb
mcgue <mcgu@protonmail.com>
2022-07-09 21:49:21 -0700

    simplify code to combine tie and winner determination in same loop, added commentary

--- rock-pap-sci-game.py
+++ rock-pap-sci-game.py
@@ -22,7 +22,7 @@ while True:
 # Commence game statement
 print('Let\'s go!')
 
-# While loop to restart when get tie
+# While loop to restart if player wants to play again
 while True:
     # Get user input
     player_choice = input('Please enter your choice: ')
@@ -38,41 +38,40 @@ while True:
     # Generate computer choice
     computer_choice = choices[random.randint(0, 2)]
 
-    # if no tie, leave this while loop
-    if player_choice != computer_choice:
-        break
-
-    # Inform there is a tie and ask whether player wants to try again
+    # Check for tie
     if player_choice == computer_choice:
         print('It is a tie!')
-    player_answer = input('Try again? Enter Y or N ')
+        print('I picked ' + computer_choice.lower() + ' too!')
+
+    # If no tie, determine winner
+    elif player_choice != computer_choice:
+        if player_choice == 'ROCK':
+            if computer_choice.upper() == 'SCISSORS':
+                print('You win :(')
+            else:
+                print('You lose, haha')
+        elif player_choice == 'PAPER':
+            if computer_choice.upper() == 'ROCK':
+                print('You win, unfortunately')
+            else:
+                print('Ha, you lose')
+        elif player_choice == 'SCISSORS':
+            if computer_choice.upper() == 'PAPER':
+                print('You win.')
+            else:
+                print('You lose!')
+        else:
+            print('still calculating')
+
+        # Print result
+        print('I picked ' + computer_choice.lower())
+
+
+    player_answer = input('Play again? Enter Y or N ')
 
     # Break if player wants to stop
-    if player_answer.upper() == 'N':
+    if player_answer.upper() != 'Y':
         break
-
-# If no tie, determine winner
-if player_choice != computer_choice:
-    if player_choice == 'ROCK':
-        if computer_choice.upper() == 'SCISSORS':
-            print('You win :(')
-        else:
-             print('You lose, haha')
-    elif player_choice == 'PAPER':
-        if computer_choice.upper() == 'ROCK':
-            print('You win, unfortunately')
-        else:
-            print('Ha, you lose')
-    elif player_choice == 'SCISSORS':
-        if computer_choice.upper() == 'PAPER':
-            print('You win.')
-        else:
-            print('You lose!')
-    else:
-        print('still calculating')
-
-    # Print result
-    print('I picked ' + computer_choice)
 
 # Sign off to end game
 print('Thanks for playing!')
